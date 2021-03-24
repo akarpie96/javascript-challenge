3b2c6df03727e880649d4652e3b2423831877203
Aaron Karpie <aaronkarpie@gmail.com>
2021-03-24 15:30:45 -0700

    filtering data correctly...so far

--- Mars-Level-1/static/js/app.js
+++ Mars-Level-1/static/js/app.js
@@ -3,21 +3,38 @@ var tableData = data;
 
 //Select the button 
 
-var button = d3.select("#filter-button")
+var button = d3.select("#filter-btn")
 
 //Select the form
 var form = d3.select("#form")
 
-// Get a reference to table body
+button.on("click", runEnter)
+form.on("submit", runEnter)
 
-var tbody=d3.select("#ufo-table");
-// YOUR CODE HERE!
-data.forEach((Aliens) => {
-  console.log(Aliens);
-  var row = tbody.append("tr");
-  Object.entries(Aliens).forEach(function([key, value]) {
-  console.log(key, value);
-  var cell = row.append("td");
-  cell.text(value)
-      });
-});
+
+//Creating Event Handlers
+//  Get a reference to table body
+// var tbody=d3.select("#ufo-table");
+// // YOUR CODE HERE!
+// data.forEach((Aliens) => {
+//   console.log(Aliens);
+//   var row = tbody.append("tr");
+//   Object.entries(Aliens).forEach(function([key, value]) {
+//   console.log(key, value);
+//   var cell = row.append("td");
+//   cell.text(value)
+//       });
+// });
+
+
+function runEnter(){
+    d3.event.preventDefault();
+    var inputElement= d3.select("#datetime");
+    var inputValue= inputElement.property("value")
+    console.log(inputValue);
+    console.log(tableData);
+
+  var filteredData = tableData.filter(alien => alien.datetime === inputValue);
+
+  console.log(filteredData);
+}
